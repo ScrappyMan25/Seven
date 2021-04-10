4b145ded1a4ba0deeef3f4a58b50418a130461cc
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-04-11 00:22:20 +0100

    Comments
    Added comments and deleted codes i dont need anymore. also Press X to Dash :^)

--- Seven/Scripts/Player.gd
+++ Seven/Scripts/Player.gd
@@ -6,7 +6,7 @@ var screen_size
 var velocity = Vector2.ZERO
 
 #Dash
-export(PackedScene) var dash_object
+export(PackedScene) var dash_object #This is only for if you want to add like a dashing effect
 export var dash_speed = 1000
 export var dash_length = 0.2
 var is_dashing : bool = false
@@ -16,6 +16,7 @@ onready var dash_timer = $dash_timer
 onready var dash_particles = $dash_particles
 
 func _ready():
+	#Establish connection with the dash timer
 	dash_timer.connect("timeout",self,"dash_timer_timeout")
 	screen_size = get_viewport_rect().size
 	pass
@@ -23,24 +24,17 @@ func _ready():
 func dash_timer_timeout():
 	is_dashing = false
 	pass
-	
+
+#gets the direction and dashes
 func get_direction_from_input():
 	var move_dir = Vector2()
-	#gets the direction of keyboard input
-#	move_dir.x = -Input.get_action_strength("ui_left") + Input.get_action_strength("ui_right")
-#	move_dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	#gets the direction of the mouse
 	move_dir = get_global_mouse_position() - position
 	move_dir = move_dir.clamped(1)
-	
-	#dash toward the direction you're facing
-#	if move_dir == Vector2(0,0):
-#		move_dir.y = -1
-#	else:
-#		move_dir.y = 1
 	return move_dir * dash_speed
 	
 func handle_dash(var delta):
-	#check if we can dash
+	#check dash - Press X to dash
 	if Input.is_action_just_pressed("dash") and can_dash:
 		is_dashing = true
 		can_dash = false
@@ -49,10 +43,10 @@ func handle_dash(var delta):
 		pass
 	if is_dashing: #Can add dash textures
 		var dash_node = dash_object.instance()
-#		dash_node.texture = (animation.frames.get_frame(animation.animation, animation.frame)
+#		dash_node.texture = (animation.frames.get_frame(animation.animation, animation.frame) <- to add textures for more effect
 		dash_node.global_position = global_position
 		get_parent().add_child(dash_node)
-		#Particles on
+		#turn particles on (currently using default particle)
 		dash_particles.emitting = true
 		if is_on_wall():
 			is_dashing = false
@@ -63,49 +57,20 @@ func handle_dash(var delta):
 	pass
 	
 func _process(delta):
-	handle_dash(delta)
 	
+	handle_dash(delta)
+	#Face toward the mouse
 	look_at(get_global_mouse_position())
 	rotation_degrees += 90
-	
-	#Movement 2 and 3
 	get_input()
-
-	#Movement 1
-#	var movement = get_input()
-#	if movement == Vector2.ZERO:
-#		apply_friction(ACCELERATION * delta)
-#	else:
-#		apply_movement(movement * ACCELERATION * delta)
 	
-	#Apply movement
 	if is_dashing:
 		velocity = move_and_slide(dash_direction)
 	else:
 		velocity = move_and_slide(velocity)
-		
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	pass
-#Movement 1
-#func apply_movement(acceleration):
-#	velocity += acceleration
-#	velocity = velocity.clamped(Speed)
-#	pass
-#
-#func apply_friction(amount):
-#	if velocity.length() > amount:
-#		velocity -= velocity.normalized() * amount
-#	else:
-#		velocity = Vector2.ZERO
-#	pass
-#
-#func get_input():
-#	var axis = Vector2.ZERO
-#	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-#	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-#	return axis.normalized()
-#	pass
 
 #Movement 2
 func get_input():
@@ -120,13 +85,3 @@ func get_input():
 		velocity -= transform.y * Speed
 	velocity = velocity.normalized() * Speed
 	pass
-
-#Movement 3
-#func get_input():
-#	velocity = Vector2.ZERO
-#	if Input.is_action_pressed("ui_down"):
-#		velocity += transform.y * Speed
-#	if Input.is_action_pressed("ui_up"):
-#		velocity -= transform.y * Speed
-#	velocity = velocity.normalized() * Speed
-#	pass
