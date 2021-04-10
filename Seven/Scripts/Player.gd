975623e8119ad9745b65c1d95764fb60929f4dad
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-04-10 16:38:04 +0100

    added 3 types of movement
    Movement 1, 2 and 3.

--- Seven/Scripts/Player.gd
+++ Seven/Scripts/Player.gd
@@ -1,7 +1,9 @@
-extends Area2D
+extends KinematicBody2D
 
 export var Speed = 400
+var ACCELERATION = 2000
 var screen_size
+var velocity = Vector2.ZERO
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -10,22 +12,64 @@ func _ready():
 func _process(delta):
 	look_at(get_global_mouse_position())
 	rotation_degrees += 90
-	var direction = get_global_mouse_position() - position
-	var normalized_direction = direction.normalized()
-	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		rotation_degrees -= 90
-		velocity = velocity * Vector2.RIGHT
-	if Input.is_action_pressed("ui_left"):
-		rotation_degrees += 90
-	if Input.is_action_pressed("ui_down"):
-		velocity = -Vector2(normalized_direction)
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2(normalized_direction)
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * Speed
-		
-	position += velocity * delta
+	
+	#Movement 2 and 3
+	
+#	get_input()
+	
+	#Movement 1
+	
+	var movement = get_input()
+	if movement == Vector2.ZERO:
+		apply_friction(ACCELERATION * delta)
+	else:
+		apply_movement(movement * ACCELERATION * delta)
+	
+	#Apply movement
+	velocity = move_and_slide(velocity)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	pass
+#Movement 1
+func apply_movement(acceleration):
+	velocity += acceleration
+	velocity = velocity.clamped(Speed)
+	pass
+	
+func apply_friction(amount):
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+	pass
+
+func get_input():
+	var axis = Vector2.ZERO
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return axis.normalized()
+	pass
+
+#Movement 2
+#func get_input():
+#	velocity = Vector2.ZERO
+#	if Input.is_action_pressed("ui_right"):
+#		velocity += transform.x * Speed
+#	if Input.is_action_pressed("ui_left"):
+#		velocity -= transform.x * Speed
+#	if Input.is_action_pressed("ui_down"):
+#		velocity += transform.y * Speed
+#	if Input.is_action_pressed("ui_up"):
+#		velocity -= transform.y * Speed
+#	velocity = velocity.normalized() * Speed
+#	pass
+
+#Movement 3
+#func get_input():
+#	velocity = Vector2.ZERO
+#	if Input.is_action_pressed("ui_down"):
+#		velocity += transform.y * Speed
+#	if Input.is_action_pressed("ui_up"):
+#		velocity -= transform.y * Speed
+#	velocity = velocity.normalized() * Speed
+#	pass
