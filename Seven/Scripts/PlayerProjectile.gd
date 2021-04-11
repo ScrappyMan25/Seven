b05a819dc953981d0d62be3d6cce984f9fa4f253
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-04-11 17:28:58 +0100

    AddedProjectile
    Projectile added

--- Seven/Scripts/PlayerProjectile.gd
+++ Seven/Scripts/PlayerProjectile.gd
@@ -7,16 +7,25 @@ var number_of_bounces = 7
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	velocity = 490
+	direction = get_global_mouse_position() - position
 	#set velocity to something 
 	#set direction to something
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
 	#move with velocity in direction
-	
+	move_and_slide(direction.normalized() * velocity)
 	#onCollision -> 
-	#print bounce number
-	#reverse direction
-	#reduce bounces
-	#reduce velocity
+	if is_on_wall():
+		var collision = move_and_collide(direction * delta)
+		#print bounce number
+		print(number_of_bounces)
+		#reverse direction
+		direction = direction.bounce(collision.normal)
+		move_and_slide(direction.normalized() * velocity)
+		#reduce bounces
+		number_of_bounces -= 1
+		#reduce velocity
+		velocity = velocity - 70
 	pass
