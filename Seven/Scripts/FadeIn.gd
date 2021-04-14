69d968a27ef89dd90018606a39374024bfd8d61f
ScrappyMan25 <53944571+ScrappyMan25@users.noreply.github.com>
2021-04-14 10:50:28 +0100

    Fixed Aspect Ratio

--- Seven/Scripts/FadeIn.gd
+++ Seven/Scripts/FadeIn.gd
@@ -6,6 +6,6 @@ func fade_in():
 	$AnimationPlayer.play("fade_in")
 	pass
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _on_AnimationPlayer_animation_finished(_anim_name):
 	emit_signal("fade_finished")
 	pass
