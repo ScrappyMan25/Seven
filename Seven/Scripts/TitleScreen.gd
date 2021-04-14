ff803b8d78395a925b6f8e859b16e6c239b7f1d2
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-04-14 19:26:31 +0100

    SomeUI

--- Seven/Scripts/TitleScreen.gd
+++ Seven/Scripts/TitleScreen.gd
@@ -5,15 +5,21 @@ var GameScene : PackedScene = preload("res://Scenes/test.tscn")
 func _ready():
 	pass # Replace with function body.
 
-func _on_Button_pressed():
-	scene_to_load = GameScene
-	$FadeIn.show()
-	$FadeIn.fade_in()
-	pass # Replace with function body.
-
 func _on_FadeIn_fade_finished():
 	queue_free()
 	var err = get_tree().change_scene_to(scene_to_load)
 	if err:
 		print(err)
+	pass # Replace with function body.
+
+
+func _on_Play_pressed():
+	scene_to_load = GameScene
+	$FadeIn.show()
+	$FadeIn.fade_in()
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
 	pass # Replace with function body.
--- Seven/Scripts/UI.gd
+++ Seven/Scripts/UI.gd
@@ -1,11 +1,26 @@
 extends CanvasLayer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$PauseButtons.hide()
+	pass # Replace with function body.
+
+
+func _on_Pause_pressed():
+	get_tree().paused = true
+	$PauseButtons.show()
+	pass # Replace with function body.
+
+
+func _on_Continue_pressed():
+	get_tree().paused = false
+	$PauseButtons.hide()
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	var err = get_tree().change_scene("res://Scenes/TitleScreen.tscn")
+	if err:
+		print(err)
+	get_tree().paused = false
 	pass # Replace with function body.
